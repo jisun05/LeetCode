e37aab7d71a3cad79028261b90add0401ca940fe
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-04-26 16:03:14 +0100

    Time: 0 ms (100%), Space: 49.4 MB (36.31%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,9 +1,8 @@
 class Solution {
     public void reverseString(char[] s) {                
-        char save =' ';
 
         for(int i =0; i < s.length / 2; i++){
-            save = s[i]; // save = h
+            char save = s[i]; // save = h
             s[i] = s[s.length-i-1];
             s[s.length-i-1] = save;
         }
