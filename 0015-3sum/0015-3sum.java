2b0a4f6e3fc696eed61e2408b70274e9f263f277
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-05-03 22:56:25 +0100

    Time: 32 ms (44.77%), Space: 51 MB (98.49%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,44 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-
-        Arrays.sort(nums);     // 정렬 
         List<List<Integer>> result = new ArrayList<>();
-   
-        for (int first = 0; first < nums.length - 2; first++) {  
+        Arrays.sort(nums);
 
-            if(first > 0 && nums[first] == nums[first-1]) continue;
-            int second = first+1;
-            int last = nums.length -1 ;
+        for (int first = 0; first < nums.length - 2; first++) {
+            if (first > 0 && nums[first] == nums[first - 1])
+                continue;
 
-            while(second < last){
-                int target = nums[first] + nums[second] + nums[last];
-                if(target ==0){
-                    result.add(Arrays.asList(nums[first], nums[second],nums[last]));
+            int second = first + 1;
+            int last = nums.length - 1;
+            while (second < last) {
+                if (nums[first] + nums[second] + nums[last] == 0) {
+                    result.add(List.of(nums[first], nums[second], nums[last]));
                     second++;
                     last--;
-                    while(second < last && nums[second] == nums [second-1]) second++;
-                    while(second < last && nums[last] == nums[last+1]) last--;
-
-                }else if(target < 0){
+                    while (second < last && nums[second] == nums[second - 1])
+                        second++;
+                    while (second < last && nums[last] == nums[last + 1])
+                        last--;
+                } else if (nums[first] + nums[second] + nums[last] < 0) {
                     second++;
-                }else{
+                } else {
                     last--;
                 }
 
-
-
-
             }
-
-        
-
-           
-            }
-
-        
+        }
 
         return result;
 
-}  
+    }
 }
