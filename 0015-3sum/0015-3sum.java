e5752af977012316a775779dbd1a43a2945462be
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-05-05 22:49:33 +0100

    Time: 31 ms (59.09%), Space: 51.6 MB (50.86%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> result = new ArrayList<>();
+   //-4  -1  -1 0 1 2 
         Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
 
         for (int first = 0; first < nums.length - 2; first++) {
-            if (first > 0 && nums[first] == nums[first - 1])
-                continue;
-
+            if(first != 0 && nums[first] == nums[first-1]) continue;
             int second = first + 1;
             int last = nums.length - 1;
             while (second < last) {
-                if (nums[first] + nums[second] + nums[last] == 0) {
-                    result.add(List.of(nums[first], nums[second], nums[last]));
-                    second++;
-                    last--;
-                    while (second < last && nums[second] == nums[second - 1])
-                        second++;
-                    while (second < last && nums[last] == nums[last + 1])
+                
+                int sum = nums[first] + nums[second] + nums[last];
+                if (sum == 0) {
+
+                    result.add(Arrays.asList(nums[first], nums[second], nums[last]));
+                        second++; last--;
+
+                      while(second< last &&nums[second] == nums[second-1]){
+                            second++;
+                      }
+                      while(second < last && nums[last] == nums[last+1] ){
                         last--;
-                } else if (nums[first] + nums[second] + nums[last] < 0) {
+                      }
+                    
+                } else if (sum < 0) {
                     second++;
-                } else {
+
+                    }
+                 else {
                     last--;
                 }
 
+              
+
+
+
+
             }
+
         }
 
         return result;
