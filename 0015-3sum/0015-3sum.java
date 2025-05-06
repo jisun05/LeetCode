23819a88f62de594890801900b641207707af27e
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-05-06 09:36:04 +0100

    Time: 32 ms (44.84%), Space: 51.5 MB (71.96%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,46 +1,49 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-   //-4  -1  -1 0 1 2 
-        Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
 
+        //first for문에서 돌리기 + first 앞뒤 값이 같을 경우
+        //while 문 돌리기 (second, last)
+        //같을 경우, 클경우, 작을 경우
+        //second 하나만 움직였는데 다음 second랑 같은경우, last 하나만 움직였는데 이전 last랑 같은경우
+         List<List<Integer>> result = new ArrayList<>();
+
+
+
+          //  -4 ,  -1  -1  0  1  2   
+          // -2 0 1 1 2
+        Arrays.sort(nums);
         for (int first = 0; first < nums.length - 2; first++) {
-            if(first != 0 && nums[first] == nums[first-1]) continue;
+            if (first != 0 && nums[first] == nums[first - 1])
+                continue;
+
             int second = first + 1;
             int last = nums.length - 1;
+           
             while (second < last) {
-                
                 int sum = nums[first] + nums[second] + nums[last];
+
                 if (sum == 0) {
-
                     result.add(Arrays.asList(nums[first], nums[second], nums[last]));
-                        second++; last--;
-
-                      while(second< last &&nums[second] == nums[second-1]){
-                            second++;
-                      }
-                      while(second < last && nums[last] == nums[last+1] ){
+                        second++;
                         last--;
-                      }
-                    
+                    while (second < last && nums[second] == nums[second - 1]) {
+                        second++;
+                    }
+                    while (second < last && nums[last] == nums[last + 1]) {
+                        last--;
+                    }
+
                 } else if (sum < 0) {
                     second++;
 
-                    }
-                 else {
+                } else {
                     last--;
                 }
 
-              
-
-
-
-
             }
-
+           
         }
-
-        return result;
+         return result;
 
     }
 }
