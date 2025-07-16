a00afb36315f0ac325fc370c48fd63c27b45226a
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-16 17:54:53 +0100

    Time: 34 ms (22.91%), Space: 55.3 MB (26.85%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -0,0 +1,37 @@
+class MyHashMap {
+    int[] map = new int[1000001];
+
+    public MyHashMap() {
+        Arrays.fill(map, -1);
+    }
+    
+    public void put(int key, int value) {
+              // do i have to make for change old value to new one?
+                map[key] = value;
+                
+
+    }
+    //I was wondering — if I initialize the entire map with -1 to indicate 'no value,' does that prevent me from storing -1 as a real value? How would I handle that case?
+    public int get(int key) {
+        //if no mapping for the key
+        if(map[key] == -1){
+            return -1;
+        }
+
+        return map[key];
+    }
+    
+    public void remove(int key) {
+        if(map[key] != -1){
+            map[key] = -1;
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
