131dc5368e6e5e487e6d0502d89f9f41a8d2284c
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-17 13:05:54 +0100

    Time: 0 ms (100%), Space: 42.6 MB (15.5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,21 +1,35 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
+        //    i      k
+        //1, 2, 3,0, 0,0
 
-          
-            for(int i =0; i < n ; i++ ){
-                nums1[m] = nums2[i];
-                m++;
-            }
-
-            Arrays.sort(nums1);
-
-
-
+             //j
+        //2,5,6
+        //first we need 3 variables
+        int i = m -1;
+        int j = n-1;
+        int k = m+n-1; //know about first inser
 
 
+            // 이 조건만 주면 될까? 조건 x
+        while(i>=0 && j >= 0){
+         if(nums1[i] > nums2[j]){
+            nums1[k] = nums1[i];
+            i--;
+         }else{
+             nums1[k] = nums2[j];
+             j--;
+         }
+        k--;
+        }
 
-
+        //think about if nums1 is 0,0,0,0   then i should be -1 then pre condition is always false
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
 
         
     }
