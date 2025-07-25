2877dfde6e8b663c6168039b9df8a24ef35e195a
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-25 13:32:35 +0100

    Time: 1 ms (99.66%), Space: 55.3 MB (87.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-
-        int n = nums.length;
-        int[] answer = new int[n];
-
-        int left = 1;  //[1,2,3,4]
-        for(int i=0; i < n ;i++){
-            answer[i] = left;
-            left *= nums[i];
-            
+        
+   //1,2,3,4,point,6,8,9
+        //앞 뒤 나눠서?
+        
+        int[] answer = new int[nums.length];
+        int left =1;
+        for(int i=0; i< nums.length; i++){
+                answer[i] = left;
+                left *= nums[i];
         }
 
         int right = 1;
-        for(int i = n-1; i>=0;i--){
+        for(int i=nums.length-1;i>=0;i--){
             answer[i] *=right;
-            right *= nums[i];
+            right *= nums[i]; 
         }
 
+
         return answer;
-        
     }
 }
