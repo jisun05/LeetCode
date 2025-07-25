0b3df8aaf44e43d51c637ced909d830ec0e769ae
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-25 20:52:14 +0100

    Time: 2 ms (71.37%), Space: 41.5 MB (66.62%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,20 +1,15 @@
 class Solution {
     public int hIndex(int[] citations) {
-
-
+        //답이 될 수 있는 최댓값은 (citations의 길이vs 값의 제일 큰값 중 작은값) 
         Arrays.sort(citations);
-
-        //0 1 3 5 6               5
-        for(int i=0; i < citations.length;i++){
-            int high = citations.length - i;
-            if(citations[i] >= high){
-                return high;
+        int check = citations.length;
+        // 0, 1, 3, 5 ,6 
+        for(int i =0; i < check;i++){
+            if(citations[i] >= check -i){
+                return check -i;
             }
-
         }
 
-
         return 0;
-        
     }
 }
