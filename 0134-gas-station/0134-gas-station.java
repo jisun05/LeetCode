915405f7c146456e206f1e15f6d04362c2e33f0b
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-25 11:48:56 +0100

    Time: 2 ms (96.93%), Space: 56.1 MB (69.76%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,30 +1,28 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-
-        int n = gas.length; //주유소
-
-        int totalTank = 0;
-        int currentTank = 0;
-        int start =0; 
-
-        for(int i =0; i < n; i++){
-            int remain = gas[i] - cost[i];
-            totalTank += remain;
-            currentTank += remain;
-
-             if(currentTank < 0){
-                start = i+1;
-                currentTank = 0;
-           
-        }
-        }
-
-        if(totalTank < 0 ) return -1;
-
-       
-
-        return start;
-
         
+        //gas[5]   -> 5th station have gas[5] gas
+        //gas[5] to gas[6] : cost[5]
+
+        int startIdx =0; //return this      
+        int totalGas =0;
+        int currentGas = 0;
+        for(int i =0; i< gas.length ; i++){
+
+            int reamin = gas[i] - cost[i];
+            totalGas += reamin;
+            currentGas += reamin;
+
+            if(currentGas <0){
+                currentGas =0;
+                startIdx= i+1;
+            }
+
+        }
+
+
+        if(totalGas < 0) return -1;
+
+        return startIdx;
     }
 }
