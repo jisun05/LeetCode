27b06f34a38de50b338bd86199fd42dd02b80fe0
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-25 08:41:32 +0100

    Time: 0 ms (100%), Space: 41.8 MB (47.85%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,16 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        
 
-        //뒤에서부터확인  "Hello World"
         int count =0;
-        for(int i = s.length()-1; i >=0;i--){
-            if(s.charAt(i) != ' '){
-                count++;
-            }else if(count !=0) break; 
-           
-        }
+        for(int i= s.length()-1;i >=0; i--){
+                if(s.charAt(i) != ' '){
+                    count++;
+                }else if(count !=0) {
+                    break;
+                }
 
+
+
+        }
         return count;
     }
 }
