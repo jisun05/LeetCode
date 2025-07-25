32cb8bbd1f0d5a02a130f581a8e8dc0fe7ef5333
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-25 22:08:27 +0100

    Time: 2 ms (29.93%), Space: 44.9 MB (93.8%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,27 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
 
+        int step =0;
+        int farthest =0;
+        int currentEnd =0;
 
-        //minimum number of jumps to reach last index
-        //nums[i] -> maximum number of jumps
-        // [2,3,1,1,4]
-        int jumps = 0;
-        int current =0;
-        int toEnd =0;
-
-        for(int i=0; i < nums.length-1;i++){
-            
-            //calculate how far can we go
-            toEnd = Math.max(toEnd, i + nums[i]);  //2,4
-
-            if(i == current){
-                jumps++;  
-                current = toEnd;    
-
+        for(int i=0 ; i < nums.length-1;i++){
+            farthest = Math.max(nums[i] + i, farthest);
+            if(i ==currentEnd){
+                step++;
+                currentEnd = farthest;
             }
         }
-
-return jumps;
-      
+       
+       return step;
+        
     }
 }
