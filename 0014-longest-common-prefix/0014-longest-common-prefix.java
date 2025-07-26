2a3e0214683cee054200f14c22ea6bd0ddbd270c
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-26 09:58:07 +0100

    Time: 0 ms (100%), Space: 41.5 MB (62.51%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,21 +1,17 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
 
-        //check first
-        String prefix = strs[0];
-       
-        for(int i=1; i < strs.length ; i++){
-            while(strs[i].indexOf(prefix) != 0 ){
-                prefix = prefix.substring(0, prefix.length()-1);
-                if(prefix.isEmpty()) return "";
+        //기준
+        String check = strs[0];
+
+        for(int i=1; i<strs.length ;i++){
+            while(strs[i].indexOf(check) != 0){
+                check = check.substring(0,check.length()-1);
             }
-            
         }
             
-        return prefix;
-        
 
-        
+            return check;
         
     }
 }
