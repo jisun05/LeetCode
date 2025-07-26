254951daebe77c87599d4cfad19bb99bae84a5e7
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-26 14:34:08 +0100

    Time: 33 ms (34.02%), Space: 51.6 MB (57.46%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,49 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        //first for문에서 돌리기 + first 앞뒤 값이 같을 경우
-        //while 문 돌리기 (second, last)
-        //같을 경우, 클경우, 작을 경우
-        //second 하나만 움직였는데 다음 second랑 같은경우, last 하나만 움직였는데 이전 last랑 같은경우
-         List<List<Integer>> result = new ArrayList<>();
+        if(nums.length < 3) return new ArrayList<>();
 
-
-
-          //  -4 ,  -1  -1  0  1  2   
-          // -2 0 1 1 2
+        List<List<Integer>> answer = new ArrayList<>();     
         Arrays.sort(nums);
-        for (int first = 0; first < nums.length - 2; first++) {
-            if (first != 0 && nums[first] == nums[first - 1])
-                continue;
+        for(int first =0; first<nums.length-2;first++){
+            if (first> 0 && nums[first] == nums[first - 1]) continue;
+            int second = first+1;
+            int third = nums.length-1; 
 
-            int second = first + 1;
-            int last = nums.length - 1;
-           
-            while (second < last) {
-                int sum = nums[first] + nums[second] + nums[last];
+            while(second<third){
+                int sum = nums[first]+ nums[second] + nums[third];
+                if(sum == 0){
+                answer.add(Arrays.asList(nums[first],nums[second],nums[third]));
+                while(second <third && nums[second] ==nums[second+1]) second++;
+                while(second <third && nums[third] == nums[third-1]) third--;
 
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[first], nums[second], nums[last]));
-                        second++;
-                        last--;
-                    while (second < last && nums[second] == nums[second - 1]) {
-                        second++;
-                    }
-                    while (second < last && nums[last] == nums[last + 1]) {
-                        last--;
-                    }
 
-                } else if (sum < 0) {
+                second++;
+                third--;
+
+                }else if(sum > 0){
+                    third--;
+                }else{
                     second++;
-
-                } else {
-                    last--;
                 }
-
             }
-           
-        }
-         return result;
 
+        }
+        return answer;
     }
 }
