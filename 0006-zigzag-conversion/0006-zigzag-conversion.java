4f2bf640ab9b2ff3d60eeda7bd9e4480814b6f6c
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-26 08:18:02 +0100

    Time: 5 ms (57.2%), Space: 45.3 MB (43.88%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,34 +1,40 @@
-    class Solution {
+class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1 || numRows >= s.length()) {
-            return s;
-        }
+
+        if(s.length() <= numRows || s.length() ==1 || numRows ==1) return s;
 
         StringBuilder[] rows = new StringBuilder[numRows];
-        for (int i = 0; i < numRows; i++) {
+
+        for(int i=0; i < numRows;i++){
             rows[i] = new StringBuilder();
         }
 
-        int curRow = 0;
-        boolean goingDown = false;
+       boolean goDown = false;
+       int currentRow = 0;
+        //알맞게 배열
 
-        for (char c : s.toCharArray()) {
-            rows[curRow].append(c);
-            if (curRow == 0) {
-                goingDown = true;
-            } else if (curRow == numRows - 1) {
-                goingDown = false;
-            }
-            curRow += goingDown ? 1 : -1;
+        for(int i=0; i <s.length(); i++){
+            rows[currentRow].append(s.charAt(i));  
+                                                   
+          if(currentRow == numRows-1){
+            goDown = false; 
+           
+          }else if(currentRow == 0){
+             goDown = true;
+          }      
+          
+          if(goDown){
+            currentRow++;
+          }else{
+            currentRow--;
+          }
         }
 
         StringBuilder result = new StringBuilder();
-        for (StringBuilder row : rows) {
-            result.append(row);
+        for (StringBuilder sb : rows) {
+        result.append(sb);
         }
-
         return result.toString();
+        
     }
-
-
 }
