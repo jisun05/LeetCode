ede2c6746cfcb953bb657b0187e6ea5b29671819
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-26 10:08:03 +0100

    Time: 0 ms (100%), Space: 41.8 MB (34.12%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+            //if(haystack.length())
+            
+            return haystack.indexOf(needle);
+
+    }
+}
