1c267a76bdc3249d8a2012cffcbe72169f8e3a82
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-26 18:37:48 +0100

    Time: 10 ms (54.44%), Space: 45.4 MB (26.6%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        //count magazine
+        for(char c : magazine.toCharArray()){
+            charCount.put(c, charCount.getOrDefault(c,0) + 1);
+        }
+
+
+        for(char c : ransomNote.toCharArray()){
+            if(!charCount.containsKey(c) || charCount.get(c) == 0){
+                return false;
+            }
+            charCount.put(c, charCount.get(c)-1);
+        }
+
+        return true;    
+    }
+}
