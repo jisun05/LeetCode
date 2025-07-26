4c7700af9785b84b7d2aee4b73bab17a8fb1d2b5
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-26 20:53:06 +0100

    Time: 1 ms (79.7%), Space: 41.9 MB (5.35%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if(words.length != pattern.length()) return false;
+
+        Map<Character, String> patternToS = new HashMap<>();
+        Map<String, Character> sToPattern = new HashMap<>();
+
+
+        for(int i=0; i<pattern.length();i++){
+
+            char c = pattern.charAt(i);
+            String w = words[i];
+
+
+            if(patternToS.containsKey(c)){//키 존재여부 확인
+                if(!patternToS.get(c).equals(w)){//키 있는데 값이 다르면
+                    return false; 
+                }
+            }
+                else{
+                    patternToS.put(c,w);
+                }
+            
+
+            if(sToPattern.containsKey(w)){
+                if(sToPattern.get(w) != c){
+                    return false;
+                }
+            }else{
+                sToPattern.put(w,c);           
+                 }
+
+
+
+
+
+        }
+
+
+
+        return true;
+    }
+}
