33be084553c486b72b298b61a2aa051c98f20947
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-27 08:47:37 +0100

    Time: 1 ms (79.7%), Space: 41.6 MB (46.73%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,44 +1,33 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] words = s.split(" ");
-        if(words.length != pattern.length()) return false;
+        // pattern = "abba", s = "dog cat cat dog"
+        String[] sSplit = s.split(" ");
+       if(pattern.length() != sSplit.length) return false;
 
-        Map<Character, String> patternToS = new HashMap<>();
-        Map<String, Character> sToPattern = new HashMap<>();
-
-
-        for(int i=0; i<pattern.length();i++){
-
-            char c = pattern.charAt(i);
-            String w = words[i];
-
-
-            if(patternToS.containsKey(c)){//키 존재여부 확인
-                if(!patternToS.get(c).equals(w)){//키 있는데 값이 다르면
-                    return false; 
-                }
+        Map<Character, String> patternToString = new HashMap<>();
+        Map<String, Character> stringToPattern = new HashMap<>();
+        for(int i=0;i< sSplit.length;i++){
+            if(patternToString.containsKey(pattern.charAt(i)) && !patternToString.get(pattern.charAt(i)).equals(sSplit[i])){
+                 return false;
             }
-                else{
-                    patternToS.put(c,w);
-                }
-            
-
-            if(sToPattern.containsKey(w)){
-                if(!sToPattern.get(w).equals(c)){
-                    return false;
-                }
-            }else{
-                sToPattern.put(w,c);           
-                 }
-
-
-
-
-
+                
+            if(stringToPattern.containsKey(sSplit[i]) && !stringToPattern.get(sSplit[i]).equals(pattern.charAt(i)) ){
+               
+            }  
+           patternToString.put(pattern.charAt(i), sSplit[i]); 
+           stringToPattern.put(sSplit[i], pattern.charAt(i));
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<pattern.length();i++){
+            sb.append(stringToPattern.get(sSplit[i]));
         }
 
+            if(!sb.toString().equals(pattern)){
+                return false;
+            }
 
+            return true;
 
-        return true;
     }
 }
