b80276f6644631add387fcd2d1da12dc4c937e78
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-27 10:40:00 +0100

    Time: 24 ms (8.82%), Space: 42.5 MB (71.22%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -10,24 +10,30 @@ class Solution {
         //s is key 
         for(int i=0 ; i< s.length();i++){
             //키가 없을떄만 넣기?
-            if(!sKeyMap.containsKey(s.charAt(i))){
+            if(sKeyMap.containsKey(s.charAt(i))){
+                if(!sKeyMap.get(s.charAt(i)).equals(t.charAt(i))) return false;
+                
+            }else{
                 sKeyMap.put(s.charAt(i), t.charAt(i));
             }   
 
-            if(!tKeyMap.containsKey(t.charAt(i))){
+            if(tKeyMap.containsKey(t.charAt(i))){
+                if(!tKeyMap.get(t.charAt(i)).equals(s.charAt(i))) return false;
+                
+            }else{
                 tKeyMap.put(t.charAt(i),s.charAt(i));
             }
         }
 
-        for(int i=0; i< t.length();i++){
-            if(!sKeyMap.get(s.charAt(i)).equals(t.charAt(i))){
-                return false;
-            }
-            if(!tKeyMap.get(t.charAt(i)).equals(s.charAt(i))){
-                return false;
-            }
+        // for(int i=0; i< t.length();i++){
+        //     if(!sKeyMap.get(s.charAt(i)).equals(t.charAt(i))){
+        //         return false;
+        //     }
+        //     if(!tKeyMap.get(t.charAt(i)).equals(s.charAt(i))){
+        //         return false;
+        //     }
 
-        }
+        // }
 
 
         return true;
