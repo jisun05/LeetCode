02c18a9e43717deb1991ba739795340f5e33373c
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-07-28 09:59:57 +0100

    Time: 2 ms (25.58%), Space: 56.9 MB (94.07%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,29 +1,26 @@
 class Solution {
     public void rotate(int[] nums, int k) {
 
-
         k = k%nums.length;
-        //nums = [1,2,3,4,5,6,7]
-        //reverse all  //7,6,5,4,3,2,1
-        for(int i=0; i< nums.length/2 ;i++){
-            int save = nums[i];
-            nums[i] = nums[nums.length-1-i];
-            nums[nums.length-1-i] = save;
-        }
+        if(nums.length > 1 ){
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
 
-        for(int i=0; i<k/2 ; i++){
-            int save = nums[i]; 
-            nums[i] = nums[k-1-i];     
-            nums[k-1-i] = save;
-        }
-
-        for(int i=nums.length-1; i>=k; i--){
-            int save = nums[i];
-            nums[i] = nums[k];
-            nums[k] = save;
-            k++;
-        }
-
-
+        } 
+        //[1,2,3,4,5,6,7]
+        //7654321
     }
+    private int[] reverse(int[] nums, int first, int last){
+            //765 4321
+            while(first<last){
+                int save =nums[first];
+                nums[first] = nums[last];
+                nums[last] =save;
+                first++;
+                last--;    
+            }
+            return nums;
+    }
+
 }
