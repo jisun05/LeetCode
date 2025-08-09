b261da903681cf29b517dad500a5ec20f2385daf
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-08-09 18:27:32 +0100

    Time: 17 ms (17.8%), Space: 45 MB (10.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-            if(s.length() != t.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        if(s.length() != t.length()){
+            return false;
+        }
 
-            Map<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
 
-
-            for(int i=0;i< s.length();i++){
-                    map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
-                    
+        for(int i=0; i< s.length();i++){
+            if(!map.containsKey(t.charAt(i)) || map.get(t.charAt(i)) == 0){
+                return false;
             }
+            map.put(t.charAt(i), map.get(t.charAt(i)) -1);
+        }
 
-            for(int i=0;i<t.length();i++){
-                if(!map.containsKey(t.charAt(i)) || map.getOrDefault(t.charAt(i),0) == 0)  {
-                    return false;
-                }else{
-                    map.put(t.charAt(i),map.get(t.charAt(i))-1);
-                }
-            
-            }
-            return true;
-
-        
+        return true;
     }
 }
