1a45f3c5bee2bb328ebe6af8f7ea57de435bab87
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-08-10 14:49:04 +0100

    Time: 0 ms (100%), Space: 40.4 MB (48.15%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,13 +1,11 @@
 class Solution {
     public int getSum(int a, int b) {
-
-        while( b != 0){
-            int current = (a&b) << 1;
-            a = a ^ b;
+   
+        while(b != 0){
+          int current  = (a & b) << 1;
+            a= a^b;
             b = current;
         }
-
         return a;
-        
     }
 }
