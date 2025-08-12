b895860420877c95747011f728a5b5c56aa63d57
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-08-12 10:19:00 +0100

    Time: 2 ms (96.56%), Space: 41.6 MB (92.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,31 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
 
-        Stack<Character> st = new Stack<>();
+        if(s.length()%2 != 0) return false;
 
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                st.push(c); // 여는 괄호는 push
-            } else { // 닫는 괄호
-                if (st.isEmpty()) return false; // 짝이 없음
-                char open = st.pop();
-                if (!matches(open, c)) return false; // 짝 불일치
-            }
-        }
-        return st.isEmpty(); // 모두 짝이 맞으면 스택 비어있음
+    Stack<Character> stack = new Stack<>();
+
+    
+   for(char c : s.toCharArray()){
+    if(c == '(' || c == '{' || c == '['){
+        stack.push(c);
+    }else{
+        if(stack.isEmpty()) return false;
+        char open = stack.pop();
+        if(!check(open,c)) return false;
+    }
     }
 
-    private boolean matches(char open, char close) {
-        return (open == '(' && close == ')')
-            || (open == '{' && close == '}')
-            || (open == '[' && close == ']');
+     return stack.isEmpty();
+   }
+    
+    
+
+ public boolean check(char open, char close){
+    if(open == '(' && close == ')') return true;
+    if(open == '{' && close =='}') return true;
+    if(open == '[' && close == ']') return true;
+    return false;
+   }   
     }
-}
+
