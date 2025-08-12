2af7c6014141f4dc980763db7a630a9f992edcf1
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-08-12 10:47:35 +0100

    Time: 409 ms (5.03%), Space: 48.1 MB (34.21%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,35 +1,28 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-          
-            
-            
-            Map<String, List<String>> map = new HashMap<>();
-            //해시맵에 숫자랑 같이 저장해서 비교
 
-            for(String s : strs){
 
-                char[] chars = new char[s.length()];
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> result = new ArrayList<>();
 
-                chars = s.toCharArray();
-
-                Arrays.sort(chars);
-                String key = new String(chars);
-
-                if(!map.containsKey(key)){
-                    map.put(key, new ArrayList());
-                }
-                    map.get(key).add(s);
-                
+        for(String s: strs){
+            char[] chars = new char[s.length()];
+            for(int i=0;i<s.length();i++){
+                chars[i] = s.charAt(i);
 
             }
-            return new ArrayList<>(map.values());
+            Arrays.sort(chars);
+            String key = new String(chars);
 
-        
-
-
-
-
-
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+                map.get(key).add(s);
+            
+           result = new ArrayList<>(map.values());
+           
+        }
+        return result;
         
     }
 }
