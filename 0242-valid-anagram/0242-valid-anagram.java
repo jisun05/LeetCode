ff008576611917026fc60dca1ce823364b2109a3
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-08-12 18:43:30 +0100

    Time: 16 ms (21.89%), Space: 45 MB (16.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
+        if(s.length() != t.length()) return false;
+
         Map<Character, Integer> map = new HashMap<>();
-        if(s.length() != t.length()){
-            return false;
-        }
 
         for(char c: s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
+            map.put(c, map.getOrDefault(c,0)+1);
         }
 
-        for(int i=0; i< s.length();i++){
-            if(!map.containsKey(t.charAt(i)) || map.get(t.charAt(i)) == 0){
+        for(char c2: t.toCharArray()){
+            if(!map.containsKey(c2) || map.get(c2) == 0){
                 return false;
+            }else{
+                map.put(c2, map.getOrDefault(c2,0)-1);
             }
-            map.put(t.charAt(i), map.get(t.charAt(i)) -1);
         }
 
         return true;
