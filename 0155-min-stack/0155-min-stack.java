a3923d9b1e2ddfdb46108c744c4c197bb82783ec
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-08-13 12:17:51 +0100

    Time: 5 ms (38.8%), Space: 45.6 MB (8.78%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,44 @@
+class MinStack {
+
+    private final Deque<Integer> stack;
+    private final Deque<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new ArrayDeque<>();
+        this.minStack = new ArrayDeque<>();
+        
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty()){
+            minStack.push(val);
+        }else{
+            int currentMin = minStack.peek();
+            minStack.push(Math.min(val, currentMin));
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
