71aa9cdaa1a68a12d40969d98bcbd1652ecbf270
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-09-06 22:12:44 +0100

    Time: 3 ms (42.42%), Space: 41.9 MB (37.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,31 +1,37 @@
 class Solution {
     public boolean isValid(String s) {
 
-        if(s.length()%2 != 0) return false;
+        if(s.length()%2 != 0){
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
 
-    Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '[' || c =='{' || c == '('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()) return false;
+                char open = stack.pop();
+                if(!check(open,c)) return false;
+            }
+            
+        }
 
-    
-   for(char c : s.toCharArray()){
-    if(c == '(' || c == '{' || c == '['){
-        stack.push(c);
-    }else{
-        if(stack.isEmpty()) return false;
-        char open = stack.pop();
-        if(!check(open,c)) return false;
-    }
-    }
+        if(stack.isEmpty()){
+            return true;
+        }
 
-     return stack.isEmpty();
-   }
-    
-    
 
- public boolean check(char open, char close){
-    if(open == '(' && close == ')') return true;
-    if(open == '{' && close =='}') return true;
-    if(open == '[' && close == ']') return true;
-    return false;
-   }   
+        return false;
     }
 
+
+    public boolean check(char open, char close){
+        if(open == '[' && close ==']') return true;
+        if(open == '(' && close ==')') return true;
+        if(open == '{' && close =='}') return true;
+
+        return false;
+    }
+
+}
