97d5babaa44f014ed1cebdbdd0ca5d2d748b5b35
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-09-09 20:11:26 +0100

    Time: 0 ms (100%), Space: 42.3 MB (41.85%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // 1. Transpose (대각선 기준으로 뒤집기)
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // 2. 각 행을 좌우 반전
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
