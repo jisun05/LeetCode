12412b54eda5f7be39adbc1511f7bcda00c82d18
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-09-10 15:20:57 +0100

    Time: 1 ms (99.74%), Space: 45.3 MB (41.07%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,20 +1,31 @@
-import java.util.*;
-
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> res = new ArrayList<>();
 
-        for (int i = 1; i <= n; i++) {
-            if (i % 15 == 0) {
-                res.add("FizzBuzz");
-            } else if (i % 3 == 0) {
-                res.add("Fizz");
-            } else if (i % 5 == 0) {
-                res.add("Buzz");
-            } else {
-                res.add(String.valueOf(i));
+        List<String> answer = new ArrayList<>(n);
+        
+
+        for(int i=0;i<n;i++){
+            int check = i+1;
+
+            if(check%3 ==0 && check%5 ==0){
+                answer.add("FizzBuzz");
+            }else if(check%3 ==0){
+                answer.add("Fizz");
+            }else if(check%5 ==0){
+                answer.add("Buzz");
+            }else{
+                answer.add(Integer.toString(check));
             }
+
         }
-        return res;
+    
+
+
+
+
+
+        return answer;
+
+        
     }
 }
