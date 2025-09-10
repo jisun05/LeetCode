58964bcfcd1776d1673c01b6d5e83df0a8f2996f
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-09-10 18:18:54 +0100

    Time: 0 ms (100%), Space: 42 MB (90.16%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,22 +2,25 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        // 1. Transpose (대각선 기준으로 뒤집기)
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int temp = matrix[i][j];
+        //대각선 기준으로 뒤집기
+        for(int i=0; i<n;i++){
+            for(int j=i+1; j<n;j++){
+                int temp =0;
+                temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
 
-        // 2. 각 행을 좌우 반전
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] = temp;
+        //좌우 반전
+        for(int i=0; i<n;i++){
+            for(int j=0; j<n/2;j++){
+                int temp2 = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp2;
+
             }
         }
+        
     }
 }
