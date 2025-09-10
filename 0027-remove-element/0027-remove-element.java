425439d72f499625d489f04def64ede6b7a6ab4b
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-09-10 22:28:17 +0100

    Time: 0 ms (100%), Space: 42.3 MB (9.09%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
 
-
+        if(nums ==null || nums.length ==0) return 0;
         int left =0;
         for(int right =0; right<nums.length; right++){
             if(nums[right] != val){
