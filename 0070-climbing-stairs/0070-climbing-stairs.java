209f48c2d274a5097258381ebf6034de13db7214
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-09-11 10:18:05 +0100

    Time: 0 ms (100%), Space: 40.4 MB (51.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
+        // 1 2 3 4
+        // 1 2 3 5
 
-        if(n <= 3) return n;
+        if(n<=3) return n;
 
-        
-        int b=1;
-        int c =2;
-        for(int i=3;i<=n;i++){
-            int result = b+c;
-            b = c;
-            c = result;
+      
+        int second =2;
+        int third =3;
+        for(int i =3; i<n;i++){
+            int sum =second + third;
+            second = third;
+            third = sum;
         }
-        return c;
+
+        return third;
+
+
     }
 }
