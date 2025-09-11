3b92998a97573e42939431b93556bd6e016597db
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-09-11 10:07:10 +0100

    Time: 0 ms (100%), Space: 40.7 MB (18.77%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public int fib(int n) {
+            
+        if(n<2) return n;
 
-        if(n < 2) return n;
-        int first= 0; 
-        int second = 1;
-            // 0 1 1 2 3 5
-        for(int i=2;i<=n;i++){
-            int third = first + second;
+        int first =0;
+        int second =1;
+        for(int i=2; i<=n;i++){
+            int third = first+second;
             first = second;
-            second = third; 
+            second = third;
+            
         }
-
         return second;
     }
 }
