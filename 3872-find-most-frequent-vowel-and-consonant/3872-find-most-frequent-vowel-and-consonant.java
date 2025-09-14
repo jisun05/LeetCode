375309c98c13563ce3fca804bace3a136ca782b9
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-09-14 15:40:58 +0100

    Time: 3 ms (61.1%), Space: 42.6 MB (53.06%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -1,7 +1,8 @@
 class Solution {
     public int maxFreqSum(String s) {
 
-            if(s.length() == 1) return 1;
+        if(s.length() == 1) return 1;
+        
         int maxVowel =0;
         int maxConsonant = 0;
         int result =0;
@@ -9,12 +10,9 @@ class Solution {
         Map<Character, Integer> mapConsonant = new HashMap<>();
         
         for(char c : s.toCharArray()){
-
         switch(c){
-
          //check vowel
         case 'a', 'e', 'i', 'o', 'u':
-
             mapVowel.put(c, mapVowel.getOrDefault(c,0)+1);
             maxVowel = Math.max(maxVowel, mapVowel.get(c));
             break;
@@ -23,11 +21,8 @@ class Solution {
             mapConsonant.put(c, mapConsonant.getOrDefault(c,0)+1);
             maxConsonant = Math.max(maxConsonant, mapConsonant.get(c));
             break;
-
         }
- 
         }
-       
             result = maxVowel+maxConsonant; 
             return result;
         
