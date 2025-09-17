427379195106134e6dc1372cf6bdaa32b09ac610
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-09-17 08:43:39 +0100

    Time: 3 ms (46.23%), Space: 41.9 MB (41.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,37 +1,33 @@
 class Solution {
     public boolean isValid(String s) {
 
+        if(s == null || s.length()%2 !=0) return false;
+        Deque<Character> deque = new ArrayDeque();
 
+        //닫는 괄호는 체크후 pop
 
-        if(s.length()%2 != 0){
-            return false;
-        }
-
-
-        Deque<Character> deque = new ArrayDeque<>();
         for(char c: s.toCharArray()){
-            if(c == '(' ||  c =='[' || c == '{'){
+            //여는 괄호들은 집어넣기
+            if(c == '(' || c == '{' || c =='['){
                 deque.push(c);
             }else{
                 if(deque.isEmpty()) return false;
-                if(!check(deque.pop(),c)) return false;
+                if(!check(deque.peek(),c)) return false;
+                deque.pop();
             }
         }
 
-        if(deque.isEmpty()){
-            return true;
-        }
+        if(!deque.isEmpty()) return false;
 
-        return false;
+        
+        return true;
     }
 
-
-
-    public boolean check(char open,char close){
+    //맞는 괄호인지 확인
+    public boolean check(char open, char close){
         if(open == '(' && close == ')') return true;
         if(open == '[' && close == ']') return true;
         if(open == '{' && close == '}') return true;
-
         return false;
     }
 }
