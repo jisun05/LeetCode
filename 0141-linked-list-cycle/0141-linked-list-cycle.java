63255f502b3d45ac7332653649bd8729d0acca8e
JISUN BAHC <130658151+jisun05@users.noreply.github.com>
2025-10-25 08:10:26 +0100

    Time: 0 ms (100%), Space: 44.4 MB (78.29%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,19 +11,20 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
-        ListNode fast = head;
+
+
         ListNode slow = head;
+        ListNode fast = head;
+
         while(fast != null && fast.next != null){
-            
-
+               slow = slow.next;
             fast = fast.next.next;
-            slow = slow.next;
-            if(fast == slow){
-                return true;
-            }
-        }
 
+            if(slow == fast) return true;
+
+         
+        }
+        
         return false;
     }
 }
